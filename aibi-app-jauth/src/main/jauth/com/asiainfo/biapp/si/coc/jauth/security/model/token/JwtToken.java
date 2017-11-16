49f8ea68600c575ab74c7e86f4691dccd944f53d
潘微微 <panww@asiainfo.com>
2017-11-16 15:41:24 +0800

    删掉迁移代码，增加忽略配置，修改groupid

--- aibi-app-jauth/src/main/jauth/com/asiainfo/biapp/si/coc/jauth/security/model/token/JwtToken.java
+++
@@ -1,5 +0,0 @@
-package com.asiainfo.biapp.si.coc.jauth.security.model.token;
-
-public interface JwtToken {
-    String getToken();
-}
--- aibi-app-jauth/src/main/jauth/com/asiainfo/biapp/si/coc/jauth/sysmgr/vo/IpWhiteListVo.java
+++
@@ -1,7 +0,0 @@
-package com.asiainfo.biapp.si.coc.jauth.sysmgr.vo;
-
-import com.asiainfo.biapp.si.coc.jauth.sysmgr.entity.IpWhiteList;
-
-public class IpWhiteListVo extends IpWhiteList{
-   
-}
