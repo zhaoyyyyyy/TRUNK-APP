bbae8240d9d8a2a1f435711567e305dc9d0d6375
张楠 <zhangnan7@asiainfo.com>
2017-11-20 19:58:02 +0800

    添加按条件查询时的条件

--- aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/core/label/labelconfig/dao/ILabelInfoDao.java
+++ aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/core/label/labelconfig/dao/ILabelInfoDao.java
@@ -1,16 +1,22 @@
 /*
  * @(#)ILabelInfoDao.java
- *
+ * 
  * CopyRight (c) 2017 北京亚信智慧数据科技有限公司 保留所有权利。
  */
 
 package com.asiainfo.biapp.si.loc.core.label.labelconfig.dao;
 
+import java.util.List;
+
+import com.asiainfo.biapp.si.loc.base.dao.BaseDao;
+import com.asiainfo.biapp.si.loc.base.page.Page;
+import com.asiainfo.biapp.si.loc.core.label.labelconfig.entity.LabelInfo;
+import com.asiainfo.biapp.si.loc.core.label.labelconfig.vo.LabelInfoVo;
 
 /**
  * Title : ILabelInfoDao
  * <p/>
- * Description : 
+ * Description :
  * <p/>
  * CopyRight : CopyRight (c) 2017
  * <p/>
@@ -18,15 +24,38 @@ package com.asiainfo.biapp.si.loc.core.label.labelconfig.dao;
  * <p/>
  * JDK Version Used : JDK 1.8
  * <p/>
- * Modification History	:
+ * Modification History :
  * <p/>
- * <pre>NO.    Date    Modified By    Why & What is modified</pre>
- * <pre>1    2017年11月16日    zhangnan7        Created</pre>
+ * 
+ * <pre>
+ * NO.    Date    Modified By    Why & What is modified
+ * </pre>
+ * 
+ * <pre>
+ * 1    2017年11月16日    zhangnan7        Created
+ * </pre>
  * <p/>
  *
- * @author  zhangnan7
+ * @author zhangnan7
  * @version 1.0.0.2017年11月16日
  */
-public interface ILabelInfoDao {
+public interface ILabelInfoDao extends BaseDao<LabelInfo, String> {
+
+    /**
+     * Description: 按条件查询分页列表
+     *
+     * @param page
+     * @param labelInfo
+     * @return
+     */
+    public Page<LabelInfo> findLabelInfoPageList(Page<LabelInfo> page, LabelInfoVo labelInfoVo);
+
+    /**
+     * Description: 按条件查询列表
+     *
+     * @param labelInfo
+     * @return
+     */
+    public List<LabelInfo> findLabelInfoList(LabelInfoVo labelInfoVo);
 
 }
