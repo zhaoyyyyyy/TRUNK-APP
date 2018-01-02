526b3ae0a552bfafb90f306b61cc9f1e7ae2d1d9
tianxy3 <tianxy3@asiainfo.com>
2018-01-02 09:17:46 +0800

    异常处理优化

--- aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/core/label/controller/ShopCartController.java
+++ aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/core/label/controller/ShopCartController.java
@@ -2,31 +2,26 @@
 package com.asiainfo.biapp.si.loc.core.label.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.asiainfo.biapp.si.loc.base.common.LabelRuleContants;
 import com.asiainfo.biapp.si.loc.base.controller.BaseController;
 import com.asiainfo.biapp.si.loc.base.exception.BaseException;
 import com.asiainfo.biapp.si.loc.base.utils.JsonUtil;
+import com.asiainfo.biapp.si.loc.base.utils.LogUtil;
 import com.asiainfo.biapp.si.loc.base.utils.StringUtil;
 import com.asiainfo.biapp.si.loc.base.utils.WebResult;
 import com.asiainfo.biapp.si.loc.cache.CocCacheProxy;
 import com.asiainfo.biapp.si.loc.core.label.entity.LabelInfo;
 import com.asiainfo.biapp.si.loc.core.label.model.ExploreQueryParam;
 import com.asiainfo.biapp.si.loc.core.label.service.ILabelExploreService;
-import com.asiainfo.biapp.si.loc.core.label.service.impl.LabelExploreServiceImpl;
 import com.asiainfo.biapp.si.loc.core.label.vo.LabelRuleVo;
 
 import io.swagger.annotations.Api;
@@ -69,10 +64,8 @@ public class ShopCartController extends BaseController {
 	@Autowired
 	private ILabelExploreService exploreServiceImpl;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ShopCartController.class);
+	private static final String SUCCESS = "success";
 
-	 private static final String SUCCESS = "success";
-	
 	/**
 	 * 添加(规则)到购物车
 	 * 
@@ -86,7 +79,7 @@ public class ShopCartController extends BaseController {
 			@ApiImplicitParam(name = "calculationsId", value = "标签ID", required = true, paramType = "query", dataType = "string"),
 			@ApiImplicitParam(name = "typeId", value = "类型", required = true, paramType = "query", dataType = "string") })
 	@RequestMapping(value = "/saveShopSession", method = RequestMethod.POST)
-	public	WebResult<String> saveShopSession(HttpServletRequest req, String calculationsId, String typeId) {
+	public WebResult<String> saveShopSession(HttpServletRequest req, String calculationsId, String typeId) {
 		WebResult<String> webResult = new WebResult<>();
 		boolean success = true;
 		String msg = "";
@@ -112,17 +105,18 @@ public class ShopCartController extends BaseController {
 					rules.add(rule);
 
 					setSessionAttribute(LabelRuleContants.SHOP_CART_RULE, JsonUtil.toJsonString(rules));
-					setSessionAttribute(LabelRuleContants.SHOP_CART_RULE_NUM,String.valueOf(findLabelOrCustomNum(rules)));
+					setSessionAttribute(LabelRuleContants.SHOP_CART_RULE_NUM,
+							String.valueOf(findLabelOrCustomNum(rules)));
 				}
 			}
 
-		} catch (BaseException e) {
-			LOGGER.error("添加(规则)到购物车异常", e);
-			return webResult.fail(e);
+		} catch (Exception e) {
+			LogUtil.error("添加(规则)到购物车异常", e);
+			return webResult.fail(e.getMessage());
 		}
 		if (success) {
 			return webResult.success("加入购物车成功", SUCCESS);
-		}else{
+		} else {
 			return webResult.fail(msg);
 		}
 	}
@@ -147,9 +141,9 @@ public class ShopCartController extends BaseController {
 				updateRules.add(rule);
 			}
 			setSessionAttribute(LabelRuleContants.SHOP_CART_RULE, JsonUtil.toJsonString(updateRules));
-		} catch (BaseException e) {
-			LOGGER.error("设置属性更新session异常", e);
-			return webResult.fail(e);
+		} catch (Exception e) {
+			LogUtil.error("设置属性更新session异常", e);
+			return webResult.fail(e.getMessage());
 		}
 		return webResult.success("更新购物车成功", SUCCESS);
 	}
@@ -184,19 +178,19 @@ public class ShopCartController extends BaseController {
 			@ApiImplicitParam(name = "monthLabelDate", value = "数据日期(月)", required = true, paramType = "query", dataType = "string") })
 	@RequestMapping(value = "/explore", method = RequestMethod.POST)
 	public WebResult<String> explore(HttpServletRequest req, String dataDate, String dayLabelDate,
-			String monthLabelDate){
+			String monthLabelDate) {
 		WebResult<String> webResult = new WebResult<>();
 		List<LabelRuleVo> labelRules = getSessionLabelRuleList();
 		ExploreQueryParam queryParam = new ExploreQueryParam(dataDate, monthLabelDate, dayLabelDate);
 		String countSqlStr = null;
 		try {
 			countSqlStr = exploreServiceImpl.getCountSqlStr(labelRules, queryParam);
-		} catch (BaseException e) {
-			return webResult.fail(e);
+			//TODO 目前直接返回数据后期调用后台接口
+		} catch (Exception e) {
+			return webResult.fail(e.getMessage());
 		}
 		return webResult.success("探索成功", countSqlStr);
-		
-		
+
 	}
 
 	/**
@@ -207,9 +201,9 @@ public class ShopCartController extends BaseController {
 		WebResult<String> webResult = new WebResult<>();
 		try {
 			setSessionAttribute(LabelRuleContants.SHOP_CART_RULE, "");
-		} catch (BaseException e) {
-			LOGGER.error("删除购物车的对象异常", e);
-			return webResult.fail(e);
+		} catch (Exception e) {
+			LogUtil.error("删除购物车的对象异常", e);
+			return webResult.fail(e.getMessage());
 		}
 		return webResult.success("删除购物车成功", SUCCESS);
 	}
@@ -256,7 +250,7 @@ public class ShopCartController extends BaseController {
 				rules = (List<LabelRuleVo>) JsonUtil.json2CollectionBean(ruleStrs, List.class, LabelRuleVo.class);
 			}
 		} catch (Exception e) {
-			LOGGER.error("getSessionLabelRuleList异常", e);
+			LogUtil.error("getSessionLabelRuleList异常", e);
 		}
 		return rules;
 	}
