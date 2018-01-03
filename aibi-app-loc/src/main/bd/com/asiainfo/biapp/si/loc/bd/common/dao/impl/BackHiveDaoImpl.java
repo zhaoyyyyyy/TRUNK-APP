44d2ca98ab3d4e60992a03ea1d83afbab15925d2
hongfb <hongfb@asiainfo.com>
2018-01-03 19:00:17 +0800

    增加后台接口抛错机制

--- aibi-app-loc/src/main/bd/com/asiainfo/biapp/si/loc/bd/common/dao/impl/BackHiveDaoImpl.java
+++ aibi-app-loc/src/main/bd/com/asiainfo/biapp/si/loc/bd/common/dao/impl/BackHiveDaoImpl.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
+import com.asiainfo.biapp.si.loc.base.exception.SqlRunException;
 import com.asiainfo.biapp.si.loc.base.utils.LogUtil;
 import com.asiainfo.biapp.si.loc.base.utils.StringUtil;
 import com.asiainfo.biapp.si.loc.bd.common.dao.IBackSqlDao;
@@ -44,14 +45,14 @@ import com.asiainfo.biapp.si.loc.bd.common.dao.IBackSqlDao;
 public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
 
 	@Override
-	public List<Map<String, String>> queryTableLikeTableName(String tableName) {
+	public List<Map<String, String>> queryTableLikeTableName(String tableName) throws SqlRunException {
 	    List<Map<String, String>> res = new ArrayList<>();
         String sql = new StringBuilder("show tables ").toString();
         List<Map<String, String>> datas = null;
         try{
             datas = this.executeResList(sql);
         }catch (Exception e){
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         if (null != datas && !datas.isEmpty()) {
@@ -67,7 +68,7 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
 	}
 
 	@Override
-	public List<Map<String, String>> queryTableColumn(String tableName) {
+	public List<Map<String, String>> queryTableColumn(String tableName) throws SqlRunException {
         List<Map<String, String>> res = new ArrayList<>();
         //desc tableName;
         String sql = new StringBuilder("desc ").append(tableName).toString();
@@ -76,7 +77,7 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
         try{
             datas = this.executeResList(sql);
         }catch (Exception e){
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         if (null != datas && !datas.isEmpty()) {
@@ -94,7 +95,7 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
 	}
 
 	@Override
-	public boolean isExistsTable(String tableName) {
+	public boolean isExistsTable(String tableName) throws SqlRunException {
         boolean res = true;
         try{
             List<Map<String, String>> cols = this.queryTableColumn(tableName);
@@ -103,14 +104,14 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             }
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         return res;
 	}
 
 	@Override
-	public boolean createTableByTemplete(String newTableName, String templeteTableName) {
+	public boolean createTableByTemplete(String newTableName, String templeteTableName) throws SqlRunException {
         boolean res = true;
         String sql = new StringBuilder("CREATE TABLE ").append(newTableName).append(" AS select a.* from ")
             .append(templeteTableName).append(" a where 1=2").toString();
@@ -119,13 +120,13 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             res = this.executeResBoolean(sql);
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         return res;
 	}
 
 	@Override
-	public boolean insertTableAsSelect(String tableName, String selectSql) {
+	public boolean insertTableAsSelect(String tableName, String selectSql) throws SqlRunException {
         boolean res = true;
         String sql = new StringBuilder("insert into table ").append(tableName).append(" ").append(selectSql).toString();
 
@@ -133,14 +134,14 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             res = this.executeResBoolean(sql);
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         return res;
 	}
 
 	@Override
-	public boolean createTableAsSelect(String tableName, String selectSql) {
+	public boolean createTableAsSelect(String tableName, String selectSql) throws SqlRunException {
 	    boolean res = true;
         String sql = new StringBuilder("CREATE TABLE ").append(tableName).append(" AS ").append(selectSql).toString();
 
@@ -148,14 +149,14 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             res = this.executeResBoolean(sql);
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
 		return res;
 	}
 
 	@Override
-	public boolean alterTable(String tableName, String columnName, String columnType) {
+	public boolean alterTable(String tableName, String columnName, String columnType) throws SqlRunException {
 	    //ALTER TABLE DIM_SCENE ADD columns(hfbcol int);
         boolean res = true;
         String sql = new StringBuilder("ALTER TABLE ").append(tableName).append(" ADD columns(").append(columnName)
@@ -165,14 +166,14 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             res = this.executeResBoolean(sql);
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         return res;
 	}
 
 	@Override
-	public List<Map<String, String>> queryForPage(String selectSql, Integer pageStart, Integer pageSize) {
+	public List<Map<String, String>> queryForPage(String selectSql, Integer pageStart, Integer pageSize) throws SqlRunException {
         int begin = (pageStart - 1) * pageSize;
         int end = begin + pageSize;
 //        String keyColumn = PropertiesUtils.getProperties("RELATED_COLUMN");
@@ -200,13 +201,12 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
         try{
             return this.executeResList(sql);
         }catch (Exception e){
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
-		return null;
 	}
 
 	@Override
-	public Integer queryCount(String selectSql) {
+	public Integer queryCount(String selectSql) throws SqlRunException {
         int rows = 0;
 //      原因是按照缺省方式打开的ResultSet不支持结果集cursor的回滚
 //      如果想要完成上述操作，要在生成Statement对象时加入如下两个参数：
@@ -225,12 +225,12 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
                 }
             }
         }catch (Exception e){
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
 		return rows;
 	}
 
-    private List<Map<String, String>> executeResList(String sql) {
+    private List<Map<String, String>> executeResList(String sql) throws SqlRunException {
         long s = System.currentTimeMillis();
         try{
             Connection connection = this.getBackConnection();
@@ -241,12 +241,11 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             
             return this.resultSetToList(resultSet);
         }catch (Exception e){
-            LogUtil.error("操作后台库出错:"+sql, e);
+            throw new SqlRunException("操作后台库出错");
         }
-        return null;
     }
 
-    private Boolean executeResBoolean(String sql) {
+    private Boolean executeResBoolean(String sql) throws SqlRunException {
         Boolean res = true;
         long s = System.currentTimeMillis();
         try{
@@ -254,18 +253,17 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             PreparedStatement preparedStatement = connection.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
             ResultSet resultSet =  preparedStatement.executeQuery();
 
-//            LogUtil.info(sql);
             LogUtil.debug(new StringBuffer(sql).append(" cost:").append(System.currentTimeMillis()-s).append("ms."));
             
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错:"+sql, e);
+            throw new SqlRunException("操作后台库出错");
         }
         return res;
     }
 
     @Override
-    public boolean dropTable(String tableName) {
+    public boolean dropTable(String tableName) throws SqlRunException {
         boolean res = true;
         //DROP TABLE pv_users;
         String sql = new StringBuilder("DROP TABLE ").append(tableName).toString();
@@ -274,14 +272,14 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             res = this.executeResBoolean(sql);
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         return res;
     }
 
     @Override
-    public boolean renameTable(String oldTableName, String newTableName) {
+    public boolean renameTable(String oldTableName, String newTableName) throws SqlRunException {
         boolean res = true;
         //ALTER TABLE oldTableName RENAME TO newTableName;
         String sql = new StringBuilder("ALTER TABLE ").append(oldTableName)
@@ -291,26 +289,26 @@ public class BackHiveDaoImpl extends BaseBackDaoImpl implements IBackSqlDao{
             res = this.executeResBoolean(sql);
         }catch (Exception e){
             res = false;
-            LogUtil.error("操作后台库出错", e);
+            throw new SqlRunException("操作后台库出错");
         }
         
         return res;
     }
 
 	@Override
-	public boolean createVerticalTable(String tableName,String columnName,String partionDate,String partionID) {
+	public boolean createVerticalTable(String tableName,String columnName,String partionDate,String partionID) throws SqlRunException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean loadDataToTabByPartion(String sql, String tableName,String partionDate,String partionID) {
+	public boolean loadDataToTabByPartion(String sql, String tableName,String partionDate,String partionID) throws SqlRunException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean insertDataToTabByPartion(String sql,String tableName,String partionDate,String partionID) {
+	public boolean insertDataToTabByPartion(String sql,String tableName,String partionDate,String partionID) throws SqlRunException {
 		// TODO Auto-generated method stub
 		return false;
 	}
