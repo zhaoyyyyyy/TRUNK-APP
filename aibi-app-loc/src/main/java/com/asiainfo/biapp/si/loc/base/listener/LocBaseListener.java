eb3b8a59abcf53bda096dd4d8c711ff334006ebd
wanghf5 <wanghf5@asiainfo.com>
2018-01-09 09:30:35 +0800

    提交刷缓存的controller代码

--- aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/base/listener/LocBaseListener.java
+++ aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/base/listener/LocBaseListener.java
@@ -5,7 +5,6 @@ import org.springframework.context.event.ContextRefreshedEvent;
 
 import com.asiainfo.biapp.si.loc.base.BaseConstants;
 import com.asiainfo.biapp.si.loc.base.LocCacheBase;
-import com.asiainfo.biapp.si.loc.core.label.dao.ILabelInfoDao;
 
 
 public class LocBaseListener implements ApplicationListener<ContextRefreshedEvent>{
@@ -14,13 +13,7 @@ public class LocBaseListener implements ApplicationListener<ContextRefreshedEven
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		
 		System.out.println("-------------------------------------LocBaseListener--------------------------------------");
-//		ILabelInfoDao iLabelInfoDao = (ILabelInfoDao)event.getApplicationContext().getBean("labelInfoDaoImpl");
 		BaseConstants.JAUTH_URL = event.getApplicationContext().getEnvironment().getProperty("jauth-url");
-//		try {
-//			RedisUtils.setAdressAndPort(LocConfigUtil.getInstance(BaseConstants.JAUTH_URL).getProperties("SYSConfig_REDIS_IP"),LocConfigUtil.getInstance(BaseConstants.JAUTH_URL).getProperties("SYSConfig_REDIS_PORT"));
-//		} catch (BaseException e) {
-//			e.printStackTrace();
-//		}
 		LocCacheBase.getInstance().init();
 		
 		
