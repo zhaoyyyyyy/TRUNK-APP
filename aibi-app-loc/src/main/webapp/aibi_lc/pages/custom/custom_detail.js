2ccc77abcee2db27c5fa4bfadfe9221d0724ed99
王瑞冬 <@10.1.48.15>
2018-01-16 14:18:42 +0800

    增加客户群详情页面

---
+++ aibi-app-loc/src/main/webapp/aibi_lc/pages/custom/custom_detail.js
@@ -0,0 +1,44 @@
+var model = {
+		labelName : "",
+		updateCycle:"",
+		orgId : "",
+		tacticsId : "",
+		busiLegend:"",	
+		configId : "",
+		dataDate:"",
+		dayLabelDate : "",
+		monthLabelDate : ""
+}
+window.loc_onload = function() {
+	var configId = $.getCurrentConfigId();
+	model.configId = configId;
+	$.commAjax({
+		postData : {
+			"configId": configId,
+			"labelId": labelId,
+		},
+		url : $.ctx + '/api/label/labelInfo/get',
+		onSuccess : function(data) {
+			model.labelName = data.data.labelName;
+			model.updateCycle = data.data.updateCycle;
+			model.orgId = data.data.orgId;
+			model.tacticsId = data.data.tacticsId;
+			model.busiLegend = data.data.busiLegend;
+			model.dataDate = data.data.dataDate;
+			model.dayLabelDate = data.data.dayLabelDate;
+			model.monthLabelDate = data.data.monthLabelDate;
+		}
+	})
+	new Vue({
+		el : '#dataD',
+		data : model,
+		mounted: function () {
+		    this.$nextTick(function () {
+			    var r = $(".easyui-validatebox");
+	   			if (r.length){
+	   				r.validatebox();
+	   			}
+		    })
+		}
+	})
+}
