5dbd8dc5591ca2d8fa532cfbb9c0e3db6cfea9cc
张楠 <zhangnan7@asiainfo.com>
2018-01-30 11:39:51 +0800

    修改全量表判断

--- aibi-app-loc/src/main/webapp/aibi_lc/pages/prefecture/allUserMsg_mgr.js
+++ aibi-app-loc/src/main/webapp/aibi_lc/pages/prefecture/allUserMsg_mgr.js
@@ -124,7 +124,7 @@ function fun_to_delete(id){
 			"priKey" : id,
 		},
 		onSuccess : function(data) {
-			if(data.data.preConfigInfoSet.length==0){
+			if(data.data.preConfigInfoSet!=null){
 				$.confirm('确定要进行[删除]吗？该操作可能会影响所有专区的数据生成', function() {
 					$.commAjax({
 						url : $.ctx + '/api/back/allUserMsg/delete',
