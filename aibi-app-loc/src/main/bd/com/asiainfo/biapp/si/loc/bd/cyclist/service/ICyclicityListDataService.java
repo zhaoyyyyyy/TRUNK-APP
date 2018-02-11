e99321996eccbb2062392f0b9b17e179da1ca967
wanghf5 <wanghf5@asiainfo.com>
2018-02-11 11:29:24 +0800

    提交周期性客户群处理service 接口

---
+++ aibi-app-loc/src/main/bd/com/asiainfo/biapp/si/loc/bd/cyclist/service/ICyclicityListDataService.java
@@ -0,0 +1,57 @@
+package com.asiainfo.biapp.si.loc.bd.cyclist.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ICyclicityListDataService {
+	
+	/**
+	 * 获取所有需要跑的日周期的周期性客户群id
+	 * @return 返回 专区id 为key，客户群id为list的 map
+	 */
+	public Map<String,List<String>> getAllDayListId();
+	
+	/**
+	 * 获取所有需要跑的月周期的周期性客户群id
+	 * @return
+	 */
+	public Map<String,List<String>> getAllMonthListId();
+	
+	/**
+	 * 处理所以有效专区的日周期清单数据
+	 */
+	public void runAllDayListData();
+	
+	/**
+	 * 处理所以有效专区的月周期清单数据
+	 */
+	public void runAllMonthListData();
+	
+	/**
+	 * 处理指定专区的日周期的清单数据
+	 * @param configId
+	 */
+	public void runDayListDataByConfigId(String configId);
+	
+	/**
+	 * 处理指定专区的月周期的清单数据
+	 * @param configId
+	 */
+	public void runMonthListDataByConfigId(String configId);
+	
+	/**
+	 * 处理指定专区的某一个周期的日周期的清单数据
+	 * @param configId
+	 * @param dataDate
+	 */
+	public void runDayListDataByConfigId(String configId,String dataDate);
+	
+	/**
+	 * 处理指定专区的某一个周期的月周期的清单数据
+	 * @param configId
+	 * @param dataDate
+	 */
+	public void runMonthListDataByConfigId(String configId,String dataDate);
+	
+
+}
---
+++ aibi-app-loc/src/main/bd/com/asiainfo/biapp/si/loc/bd/cyclist/service/impl/CyclicityListDataServiceImpl.java
@@ -0,0 +1,58 @@
+package com.asiainfo.biapp.si.loc.bd.cyclist.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.asiainfo.biapp.si.loc.bd.cyclist.service.ICyclicityListDataService;
+
+public class CyclicityListDataServiceImpl implements ICyclicityListDataService{
+
+	@Override
+	public Map<String, List<String>> getAllDayListId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, List<String>> getAllMonthListId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void runAllDayListData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runAllMonthListData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runDayListDataByConfigId(String configId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runMonthListDataByConfigId(String configId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runDayListDataByConfigId(String configId, String dataDate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runMonthListDataByConfigId(String configId, String dataDate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
