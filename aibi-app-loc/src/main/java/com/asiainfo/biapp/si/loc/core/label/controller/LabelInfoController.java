4aae6c4a4bb3cfdc25232b3ac489923651fd2ae5
tianxy3 <tianxy3@asiainfo.com>
2018-03-08 14:02:29 +0800

    用户名保存

--- aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/core/label/controller/LabelInfoController.java
+++ aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/core/label/controller/LabelInfoController.java
@@ -129,7 +129,7 @@ public class LabelInfoController extends BaseController {
 			labelInfo.setBusiLegend(busiLegend);
 			labelInfo.setConfigId(configId);
 			labelInfo.setDataDate(dataDate);
-			labelInfo.setCreateUserId(getLoginUser().getUserId());
+			labelInfo.setCreateUserId(getLoginUser().getUserName());
 			// 拓展信息
 			LabelExtInfo labelExtInfo = new LabelExtInfo();
 	        labelExtInfo.setTacticsId(tacticsId);
