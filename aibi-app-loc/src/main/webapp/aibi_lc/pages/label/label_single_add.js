d097cbedbb2300784defcb58f138b9740135e50c
张楠 <zhangnan7@asiainfo.com>
2018-03-19 18:20:29 +0800

    修改无法注册周期

--- aibi-app-loc/src/main/webapp/aibi_lc/pages/label/label_single_add.js
+++ aibi-app-loc/src/main/webapp/aibi_lc/pages/label/label_single_add.js
@@ -145,7 +145,8 @@ function fun_to_save(){
 	$("form[class~=active]").each(function(){
 		if($("form[class~=active]").validateForm()){	
 			var labelInfo = $(this).formToJson();
-			labelInfo["updateCycle"] = model.updateCycle;
+			var uc = model.readCycle;
+			labelInfo.updateCycle = uc;
 			$.commAjax({
 			  async : false,
 			  url : $.ctx + '/api/label/labelInfo/save',
