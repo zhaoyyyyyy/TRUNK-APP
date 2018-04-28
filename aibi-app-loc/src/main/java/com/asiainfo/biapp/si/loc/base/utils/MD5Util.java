acbf1936e6a7526a9a850ade3340fb1b6fa348de
hongfb <hongfb@asiainfo.com>
2018-04-28 14:18:31 +0800

    [修复] sonar扫描

--- aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/base/utils/MD5Util.java
+++ aibi-app-loc/src/main/java/com/asiainfo/biapp/si/loc/base/utils/MD5Util.java
@@ -15,8 +15,6 @@ import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import org.apache.log4j.Logger;
-
 /**
  * Title : MD5Util
  * <p/>
@@ -41,8 +39,9 @@ import org.apache.log4j.Logger;
  */
 
 public class MD5Util {
-	private static final Logger log = Logger.getLogger(MD5Util.class);
-	
+    public static final int BINARY = 2;
+    public static final int BINARY_1024 = 1024;
+    
 	/**
 	 * 默认的密码字符串组合，用来将字节转换成 16 进制表示的字符,apache校验下载的文件的正确性用的就是默认的这个组合
 	 */
@@ -94,19 +93,19 @@ public class MD5Util {
 		InputStream fis = null;
 		try {
 			fis = new FileInputStream(file);
-			byte[] buffer = new byte[1024];
+			byte[] buffer = new byte[BINARY_1024];
 			int numRead = 0;
 			while ((numRead = fis.read(buffer)) > 0) {
 				messagedigest.update(buffer, 0, numRead);
 			}
 		} catch (Exception e) {
-			log.error("获取文件MD5校验码失败", e);
+			LogUtil.error("获取文件MD5校验码失败", e);
 		} finally {
 			if (fis != null) {
 				try {
 					fis.close();
 				} catch (IOException e) {
-					log.error("关闭文件流异常", e);
+				    LogUtil.error("关闭文件流异常", e);
 				}
 			}
 		}
@@ -135,20 +134,20 @@ public class MD5Util {
 					file.length());
 			messagedigest.update(byteBuffer);
 		} catch (Exception e) {
-			log.error("获取文件MD5校验码失败", e);
+		    LogUtil.error("获取文件MD5校验码失败", e);
 		} finally {
 			if (ch != null) {
 				try {
 					ch.close();
 				} catch (IOException e) {
-					log.error("关闭文件流异常", e);
+				    LogUtil.error("关闭文件流异常", e);
 				}
 			}
 			if (fis != null) {
 			    try {
 			        fis.close();
 			    } catch (IOException e) {
-			        log.error("关闭文件流异常", e);
+			        LogUtil.error("关闭文件流异常", e);
 			    }
 			}
 		}
@@ -165,21 +164,21 @@ public class MD5Util {
 	}
 
 	private static String bufferToHex(byte bytes[], int m, int n) {
-		StringBuffer stringbuffer = new StringBuffer(2 * n);
+	    StringBuilder stringBuilder = new StringBuilder(BINARY * n);
 		int k = m + n;
 		for (int l = m; l < k; l++) {
-			appendHexPair(bytes[l], stringbuffer);
+			appendHexPair(bytes[l], stringBuilder);
 		}
-		return stringbuffer.toString();
+		return stringBuilder.toString();
 	}
 
-	private static void appendHexPair(byte bt, StringBuffer stringbuffer) {
+	private static void appendHexPair(byte bt, StringBuilder stringBuilder) {
 		// 取字节中高 4 位的数字转换, >>> 为逻辑右移，将符号位一起右移,此处未发现两种符号有何不同
-		char c0 = hexDigits[(bt & 0xf0) >> 4];
+		char c0 = hexDigits[(bt & 0xf0) >> (BINARY*BINARY)];
 		// 取字节中低 4 位的数字转换
 		char c1 = hexDigits[bt & 0xf];
-		stringbuffer.append(c0);
-		stringbuffer.append(c1);
+		stringBuilder.append(c0);
+		stringBuilder.append(c1);
 	}
 
 	public static void main(String[] args) throws IOException {
